63de4726b080d71c57dc06b0365422eab23e1e59
Even Solbraa <esol@statoil.com>
2018-03-22 14:48:24 +0100

    installer

---
+++ NeqSimRegistration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeqSimRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ThermoPackages tempVar = new ThermoPackages();
+            //Object test = tempVar.GetPropertyPackageList();
+            Type t = null;
+            CapeOpenThermo.ThermoPackageManagerCO11.RegisterFunction(t);
+            CapeOpenThermo.ThermoPackageManagerCO10.RegisterFunction(t);
+            System.Console.WriteLine("finished registering NeqSim CapeOpen2....");
+            //CapeOpenUnitOperations.HydrateEquilibriumUnitOperation.RegisterFunction(t);
+            //   CapeOpenUnitOperations.WaterDewPointUnitOperation.RegisterFunction(t);
+            //  CapeOpenUnitOperations.FreezingUnitOperation.RegisterFunction(t);
+            //  CapeOpenUnitOperations.ProCapMixerUnitOperation.RegisterFunction(t);
+            //  CapeOpenUnitOperations.UnitOperationBaseClass.RegisterFunction(t);
+
+            System.Console.WriteLine("finished registering NeqSim CapeOpen....");
+            System.Console.ReadLine();
+            // testing..
+            //testing2
+            //testing3
+            //testing4
+        }
+    }
+}
